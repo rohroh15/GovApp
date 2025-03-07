01fa0c280773b7baf034b7c525f95523625d30c6
rohroh15 <rohit.kkfe@outlook.com>
2025-03-07 14:10:03 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import App from "./App";
+import Home from "./Home";
+import Meetings from "./Meetings";
+import Agenda from "./Agenda";
+import Summary from "./Summary";
+import Settings from "./Settings";
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/meetings" element={<Meetings />} />
+        <Route path="/agenda/:meetingId" element={<Agenda />} />
+        <Route path="/summary" element={<Summary />} />
+        <Route path="/settings" element={<Settings />} />
       </Routes>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
-
